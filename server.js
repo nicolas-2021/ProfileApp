1ccf3f182be305099697e400bfd5ae2660f3ba61
Nicholas <nicolasmartinamuedo1@gmail.com>
2023-11-25 19:46:37 -0300

    fix: listening chat message event from the client socket

--- server.js
+++ server.js
@@ -21,14 +21,45 @@ const auth = require('./auth.js');
 
 const { ObjectID } = require('mongodb');
 
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  store: store,
+  cookie: { secure: false },
+  key: 'express.sid'
 }));
 
 app.use(passport.initialize());
@@ -39,13 +70,22 @@ myDB(async client => {
  
   auth(app,myDataBase);
   routes(app,myDataBase);
+
   let currentUsers = 0;
   io.on('connection', socket => {
     console.log('A user has connected');
-    currentUsers+=1;
-    io.emit('user count', currentUsers);
+    console.log('user ' + socket.request.user.username + ' connected');
+    ++currentUsers;
+    io.emit('user', {username: socket.request.user.username,currentUsers,connected: true});
+    socket.on('disconnect', () => {
+      --currentUsers;
+      io.emit('user', {username: socket.request.user.username,currentUsers,connected: false}); 
+    });
+    socket.on('chat message', (message) => {
+      io.emit('chat message', {username: socket.request.user.username, message: message});
+    })
   });
-
+ 
 
   // Be sure to change the title
 
